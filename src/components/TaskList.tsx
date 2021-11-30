cefa902ae5f2e64cec48ccb6ee278502a1e7b39c
Dannyllo Baltazar <dannyllo.souza@tsea.com.br>
2021-11-30 00:19:13 -0300

    Solução Desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    var task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks([...tasks, task]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    var _tasks = tasks.map(t => t.id === id ? {
+      ...t,
+      isComplete: !t.isComplete
+    } : t)
+
+    setTasks(_tasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    var _tasks = tasks.filter(t => t.id !== id);
+
+    setTasks(_tasks);
   }
 
   return (
